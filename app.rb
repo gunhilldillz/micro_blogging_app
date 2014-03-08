6217ef51adcb981a87ab4a05a592d8d5ded091f5
Joseph Rivera <josephrivera@jmr1.com>
2014-03-08 17:02:18 -0500

    functioning

--- app.rb
+++ app.rb
@@ -7,9 +7,11 @@ configure(:development) {
 	set :database, "sqlite3:///mba.sqlite3"
 }
 
-#set :database, 'sqlite3:///mba.sqlite3'  #add database file
+#set :database, 'sqlite3:///mba.sqlite3'  #add database file # no longer needed, only set above in development environment
+
 set :sessions, true
 set :environment, :developmennt
+
 use Rack::Flash, :sweep => true
 
 # * load Ruby files
@@ -19,6 +21,7 @@ use Rack::Flash, :sweep => true
 
 Dir['./*.rb','./models/*.rb'].each{ |f| require f }
 
+# METHODS
 
 def current_user
 	if session[:user_id]
@@ -32,3 +35,44 @@ get "/" do
 
 end
 
+
+
+
+post "/sign-in" do
+  @user = User.where(username: params[:username] ).first
+
+  if @user && ( @user.password == params[:password] )
+    puts "Yay! Signed in!"
+    #store the user id in the session
+    session[:user_id] = @user.id
+
+    #notify the user that they are signed in
+    flash[:notice] = "You are signed in!"
+
+    redirect to "/signed_in"
+  
+  else
+
+    flash[:error] = "Unable to sign you in!"
+    redirect to "/"
+  end
+end
+
+get "/signed_in" do
+  flash[:notice] = "You are signed in!"
+  erb :signed_in
+end
+
+post "/create_post" do
+  puts params
+  if params[:post][:title] && params[:post][:body]
+    #make the Post in here...
+    @post = Post.create(params[:post])
+
+    if @post
+      redirect to("/signed_in")
+      else
+    redirect to("/signed_in")
+  end
+end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228011725) do
+ActiveRecord::Schema.define(version: 20140308200712) do
+
+  create_table "posts", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string "username"
@@ -20,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140228011725) do
     t.string "lname"
     t.string "gender"
     t.date   "birthday"
+    t.string "password"
   end
 
 end
